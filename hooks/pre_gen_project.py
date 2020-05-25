9b045df40487f111f6ac9c1b82a48c25c612f51f
Tyler Richardson <tyl0rz@gmail.com>
2020-05-25 02:36:47 +0000

    add check for existing repo

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,14 +1,25 @@
 import re
+import requests
 import sys
 
 
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
 module_name = '{{ cookiecutter.project_slug }}'
+github_username = '{{ cookiecutter.github_username }}'
 
 if not re.match(MODULE_REGEX, module_name):
     print(('ERROR: The project slug (%s) is not a valid Python module name. Please do not use'
            ' a - and use _ instead' % module_name))
 
     # Exit to cancel project
+    sys.exit(1)
+
+response = requests.get(f"https://api.github.com/repos/{github_username}/{module_name}")
+
+try:
+    response.raise_for_status()
+except requests.exceptions.HTTPError:
+    print(('ERROR: The repository (%s) already exists. Please select a new project name.' % f"{github_username}/{module_name}"))
+
     sys.exit(1)
